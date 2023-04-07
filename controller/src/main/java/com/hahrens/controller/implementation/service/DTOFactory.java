a7e7edeed9317e6de4c7a3128e1a1be29f052dd7
HenneGit <he.ahrens@gmail.com>
2023-04-07 07:53:01 +0200

    cleanup and comments.

--- controller/src/main/java/com/hahrens/controller/implementation/service/DTOFactory.java
+++ controller/src/main/java/com/hahrens/controller/implementation/service/DTOFactory.java
@@ -3,7 +3,6 @@ package com.hahrens.controller.implementation.service;
 import com.hahrens.backend.model.AnswerEntity;
 import com.hahrens.backend.model.QuestionEntity;
 import com.hahrens.backend.model.SurveyEntity;
-import com.hahrens.controller.api.model.container.QuestionContainer;
 import com.hahrens.controller.api.model.dto.AnswerDTO;
 import com.hahrens.controller.api.model.dto.QuestionDTO;
 import com.hahrens.controller.api.model.dto.SurveyDTO;
@@ -11,8 +10,6 @@ import com.hahrens.controller.implementation.model.AnswerDTOImpl;
 import com.hahrens.controller.implementation.model.QuestionDTOImpl;
 import com.hahrens.controller.implementation.model.SurveyDTOImpl;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 /**
@@ -26,39 +23,33 @@ final class DTOFactory {
 
     /**
      * produce question dto from questionEntity.
-     * @param questionEntity
-     * @param surveyPk
-     * @return
+     * @param questionEntity the questionEntity to create dto from.
+     * @param surveyPk the survey pk this question belongs to.
+     * @return the dto object.
      */
-    static QuestionDTO getQuestionDTO(QuestionEntity questionEntity, UUID surveyPk) {
+    static QuestionDTO getQuestionDTO(final QuestionEntity questionEntity, final UUID surveyPk) {
         UUID primaryKey = UUID.randomUUID();
-        List<AnswerEntity> answers = questionEntity.getAnswers();
-        List<AnswerDTO> answerDTOS = new ArrayList<>();
-        if (answers != null) {
-            for (AnswerEntity answer : answers) {
-                answerDTOS.add(getAnswerDTO(answer, primaryKey));
-            }
-        }
         return new QuestionDTOImpl(primaryKey, questionEntity.getName(), questionEntity.getDescription(), questionEntity.getQuestion(), surveyPk);
-
     }
 
-    static AnswerDTO getAnswerDTO(AnswerEntity answerEntity, UUID questionPk) {
+    /**
+     * produce a answerDTO from answerEntity.
+     * @param answerEntity the entity to create dto from.
+     * @param questionPk the question this answer belongs to.
+     * @return the dto object.
+     */
+    static AnswerDTO getAnswerDTO(final AnswerEntity answerEntity, final UUID questionPk) {
         return new AnswerDTOImpl(UUID.randomUUID(), questionPk, answerEntity.getAnswerText());
 
     }
 
+    /**
+     * produce a survey dto from surveyEntity.
+     * @param surveyEntity the surveyEntity to  create dto from.
+     * @return the dto object.
+     */
     static SurveyDTO getSurveyDTO(final SurveyEntity surveyEntity) {
         UUID primaryKey = UUID.randomUUID();
-        List<QuestionEntity> questionEntities = surveyEntity.getQuestionEntities();
-        List<QuestionDTO> questionDTOS = new ArrayList<>();
-        if (questionEntities != null) {
-
-            for (QuestionEntity questionEntity : questionEntities) {
-                questionDTOS.add(getQuestionDTO(questionEntity, primaryKey));
-            }
-        }
-        QuestionContainer questionContainer = new QuestionContainerImpl(questionDTOS);
         return new SurveyDTOImpl(primaryKey, surveyEntity.getName(), surveyEntity.getDescription());
     }
 
--- frontend/src/main/reactapp/polly-frontend/src/services/QuestionService.ts
+++ frontend/src/main/reactapp/polly-frontend/src/services/QuestionService.ts
@@ -2,29 +2,30 @@ import {Question} from "../model/models";
 import http from "./http-common"
 
 
-const getAll = () => {
-    return http.get<Array<Question>>("/question/get");
+const getAll = async () => {
+    return await http.get<Array<Question>>("/question/get");
 };
 
-const get = (id: string) => {
-    return http.get<Question>(`/question/${id}`);
+const get = async (id: string) => {
+    return await http.get<Question>(`/question/${id}`);
 };
 
-const add = (data: Question) => {
-    return http.post<Question>("/question/add", data);
+const add = async (data: Question) => {
+    return await http.post<Question>("/question/add", data);
 };
 
-const update = (data: Question) => {
-    return http.put<Question>(`/question/update`, data);
+const update = async (data: Question) => {
+    return await http.put<Question>(`/question/update`, data);
 };
 
-const remove = (id: string) => {
-    return http.delete<any>(`/question/delete/${id}`);
+const remove = async (id: string) => {
+    return await http.delete<any>(`/question/delete/${id}`);
 };
 
-const getBySurveyId = (id: string) => {
-    return http.get<Question>(`/question/getBySurveyId/${id}`);
+const getBySurveyId = async (id: string) => {
+    return await http.get<Question>(`/question/getBySurveyId/${id}`);
 };
+
 
 
 const QuestionService = {
