d41f23e913eae7700ebb3daadc196d1201e81aec
HenneGit <he.ahrens@gmail.com>
2023-04-09 09:08:22 +0200

    description editing added. Correct update not working yet.

--- frontend/src/main/reactapp/polly-frontend/src/component/survey/SingleSurvey.tsx
+++ frontend/src/main/reactapp/polly-frontend/src/component/survey/SingleSurvey.tsx
@@ -1,8 +1,8 @@
-import React, {Dispatch, SetStateAction, useEffect, useReducer, useRef, useState} from "react";
+import React, {useRef, useState} from "react";
 import {Survey} from "../../model/models";
 import {AiFillDelete, AiFillEdit} from "react-icons/ai";
+import {MdDone} from "react-icons/md";
 import "./SingleSurvey.css"
-import {SURVEY_ACTION_TYPE, surveyReducer} from "./SurveyReducer";
 import SurveyService from "../../services/SurveyService";
 
 type Props = {
@@ -12,23 +12,44 @@ type Props = {
 
 const SingleSurvey = ({survey, dispatch}: Props) => {
     const [edit, setEdit] = useState<boolean>(false);
-    const [editSurvey, setEditSurvey] = useState<Survey>(survey);
+    const [newName, setEditSurveyName] = useState<string>(survey.name);
+    const [newDescription, setEditSurveyDescription] = useState<string>(survey.description);
     const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleUpdate = () => {
+        SurveyService.update({primaryKey: survey.primaryKey, name: newName, description: newDescription}, dispatch)
+        setEdit(false);
+    }
 
 
     return (
-        <form className='survey__single' onSubmit={(e) => {SurveyService.update(editSurvey, dispatch)}}>
+        <form className='survey__single' onSubmit={(e) => {
+        }}>
             {edit ? (
-                <input
-                    ref={inputRef}
-                    value={editSurvey.name}
-                    onChange={(e) => setEditSurvey({primaryKey: survey.primaryKey, name:e.target.value, description:survey.description})}
-                    className="survey__single--text"
-                />
+                <div className='survey__single--details'>
+                    <input
+                        ref={inputRef}
+                        value={newName}
+                        onChange={(e) => setEditSurveyName(e.target.value)}
+                        className="survey__single--text"
+                    />
+                    <input
+                        ref={inputRef}
+                        value={newDescription}
+                        onChange={(e) => setEditSurveyDescription(e.target.value)}
+                        className="survey__single--text"
+                    />
+                    <span className="icon" onClick={() => handleUpdate()}>
+                    <MdDone/>
+                </span>
+                </div>
             ) : (
-                <span className="survey__single--text">{survey.name}</span>
+                <div className='survey__single--details'>
+                    <span className="survey__single--text">{survey.name}</span>
+                    <span className="survey__single--description">{survey.description}</span>
+                </div>
             )}
-            <div>
+            <div className='survey__single--icons'>
             <span
                 className="icon"
                 onClick={() => {
@@ -40,8 +61,8 @@ const SingleSurvey = ({survey, dispatch}: Props) => {
               <AiFillEdit/>
             </span>
                 <span className="icon" onClick={() => SurveyService.remove(survey.primaryKey, dispatch)}>
-              <AiFillDelete/>
-            </span>
+                    <AiFillDelete/>
+                </span>
             </div>
         </form>
     )
